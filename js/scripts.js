565031d82c0773256fa5af94f7e002f545e22373
candycrushpro <bradleynilmar6702@gmail.com>
2020-02-24 17:23:44 +0300

    h4 changes on footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
+      // $("#development-image").slideDown('1500').hide('1000');
       $("#development").show('1500');
     });
     $("#development").click(function(){
@@ -23,7 +23,7 @@ $(document).ready(function(){
   
   $(document).ready(function(){
     $("#product-image").click(function(){
-      $("#product-image").slideDown('1500').hide('1000');
+      // $("#product-image").slideDown('1500').hide('1000');
       $("#product").show('1500');
     });
     $("#product").click(function(){
@@ -34,7 +34,7 @@ $(document).ready(function(){
   
   $(document).ready(function(){
     $("form#contact-me").submit(function(event){
-      // event.preventDefault();
+      event.preventDefault();
       var name = $("input#MERGE1").val();
       var email = $("input#MERGE0").val();
       var message = $("textarea#comment").val();

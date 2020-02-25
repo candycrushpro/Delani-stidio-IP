9632ce31b89c7b18ffcd2f680a84cfbea7b502dc
candycrushpro <bradleynilmar6702@gmail.com>
2020-02-25 06:16:38 +0300

    final changes on portfolio hovering

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $("#development-image").click(function(){
-      // $("#development-image").slideDown('1500').hide('1000');
+      $("#development-image").toggle('1500')
       $("#development").show('1500');
     });
     $("#development").click(function(){
-      $("#development-image").slideDown('1500');
+      $("#development-image").toggle('1500');
       $("#development").slideUp('1500');
       
     });
@@ -12,28 +12,79 @@ $(document).ready(function(){
 
   $(document).ready(function(){
     $("#design-image").click(function(){
-      $("#design-image").slideDown('1500').hide('1000');
+      $("#design-image").toggle('1500').hide('1000');
       $("#design").show('1500');
     });
     $("#design").click(function(){
-      $("#design").slideUp('1500');
+      $("#design").toggle('1500');
       $("#design-image").slideDown('1500');
     });
   });
   
   $(document).ready(function(){
     $("#product-image").click(function(){
-      // $("#product-image").slideDown('1500').hide('1000');
+      $("#product-image").toggle('1500').hide('1000');
       $("#product").show('1500');
     });
     $("#product").click(function(){
-      $("#product").slideUp('1500');
+      $("#product").toggle('1500');
       $("#product-image").slideDown('1500');
     });
   });
+
+
+
+
+  // $(document).ready(function(){
+  //   $("#work1").mouseover(function(){
+  //     $("#overlay"):hover();
+  //   }).mouseout(function(){
+  //     $("#overlay").hide();
+  //   });
+
+  //   $("#work2").mouseover(function(){
+  //     $("#overlay2").show();
+  //   }).mouseout(function(){
+  //     $("#overlay2").hide();
+  //   });
+  
+  //   $("#work3").mouseover(function(){
+  //     $("#overlay3").show();
+  //   }).mouseout(function(){
+  //     $("#overlay3").hide();
+  //   });
+
+  //   $("#work4").mouseover(function(){
+  //     $("#overlay4").show();
+  //   }).mouseout(function(){
+  //     $("#overlay4").hide();
+  //   });
+  
+  //   $("#work5").mouseover(function(){
+  //     $("#overlay5").show();
+  //   }).mouseout(function(){
+  //     $("#overlay5").hide();
+  //   });
+  //   $("#work6").mouseover(function(){
+  //     $("#overlay6").show();
+  //   }).mouseout(function(){
+  //     $("#overlay6").hide();
+  //   });
+  //   $("#work7").mouseover(function(){
+  //     $("#overlay7").show();
+  //   }).mouseout(function(){
+  //     $("#overlay7").hide();
+  //   });
+  //   $("#work8").mouseover(function(){
+  //     $("#overlay8").show();
+  //   }).mouseout(function(){
+  //     $("#overlay8").hide();
+  //   });
+  // });
+  
   
   $(document).ready(function(){
-    $("form#contact-me").submit(function(event){
+    $("form.contact-me").submit(function(event){
       event.preventDefault();
       var name = $("input#MERGE1").val();
       var email = $("input#MERGE0").val();

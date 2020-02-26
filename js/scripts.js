5747a2fabac73d54360cbcd202a9e03b1423570c
candycrushpro <bradleynilmar6702@gmail.com>
2020-02-26 09:17:37 +0300

    updates..

--- js/scripts.js
+++ js/scripts.js
@@ -30,58 +30,7 @@ $(document).ready(function(){
       $("#product").toggle('1500');
       $("#product-image").slideDown('1500');
     });
-  });
-
-
-
-
-  // $(document).ready(function(){
-  //   $("#work1").mouseover(function(){
-  //     $("#overlay"):hover();
-  //   }).mouseout(function(){
-  //     $("#overlay").hide();
-  //   });
-
-  //   $("#work2").mouseover(function(){
-  //     $("#overlay2").show();
-  //   }).mouseout(function(){
-  //     $("#overlay2").hide();
-  //   });
-  
-  //   $("#work3").mouseover(function(){
-  //     $("#overlay3").show();
-  //   }).mouseout(function(){
-  //     $("#overlay3").hide();
-  //   });
-
-  //   $("#work4").mouseover(function(){
-  //     $("#overlay4").show();
-  //   }).mouseout(function(){
-  //     $("#overlay4").hide();
-  //   });
-  
-  //   $("#work5").mouseover(function(){
-  //     $("#overlay5").show();
-  //   }).mouseout(function(){
-  //     $("#overlay5").hide();
-  //   });
-  //   $("#work6").mouseover(function(){
-  //     $("#overlay6").show();
-  //   }).mouseout(function(){
-  //     $("#overlay6").hide();
-  //   });
-  //   $("#work7").mouseover(function(){
-  //     $("#overlay7").show();
-  //   }).mouseout(function(){
-  //     $("#overlay7").hide();
-  //   });
-  //   $("#work8").mouseover(function(){
-  //     $("#overlay8").show();
-  //   }).mouseout(function(){
-  //     $("#overlay8").hide();
-  //   });
-  // });
-  
+  }); 
   
   $(document).ready(function(){
     $("form.contact-me").submit(function(event){
